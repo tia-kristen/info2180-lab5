d29bc7c08f926323623ca1dea0207bd328fbc6f0
tia <tiakristensalmon@gmail.com>
2022-11-11 12:13:25 -0500

    Display results as table instead of list

--- world.php
+++ world.php
@@ -9,10 +9,21 @@ $country = filter_input(INPUT_GET, 'query');
 $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%';");
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
 ?>
-<ul>
+
+<table>
+  <tr>
+    <th>Name</th>
+    <th>Continent</th>
+    <th>Independence</th>
+    <th>Head of State</th>
+  </tr>
 <?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
+  <tr>
+    <td><?php echo $row['name'] ?></td>
+    <td><?php echo $row['continent'] ?></td>
+    <td><?php echo $row['independence_year'] ?></td>
+    <td><?php echo $row['head_of_state'] ?></td>
+  </tr>
 <?php endforeach; ?>
-</ul>
+</table>
