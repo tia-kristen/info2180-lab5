654ce43665ed0172c597d64bcec007d063169c21
tia <tiakristensalmon@gmail.com>
2022-11-14 14:34:31 -0500

    Implemented cities lookup

--- world.js
+++ world.js
@@ -1,12 +1,27 @@
 window.onload = () => {
     const lookupBtn = document.querySelector('#lookup');
+    const lookupCitiesBtn = document.querySelector('#lookup-cities');
     const input = document.querySelector('#country');
     const result = document.querySelector('#result');
 
     lookupBtn.addEventListener('click', async (e) => {
         e.preventDefault();
-        let query = input.value;
-        let response = await fetch(`world.php?query=${query}`);
+        let country = input.value;
+        let response = await fetch(`world.php?query=${country}`);
+
+        if(response.status === 200){
+            let data = await response.text();
+            result.innerHTML = data;
+        } else {
+            alert("There was a problem processing your request.");
+        }
+        
+    })
+
+    lookupCitiesBtn.addEventListener('click', async (e) => {
+        e.preventDefault();
+        let country = input.value;
+        let response = await fetch(`world.php?query=${country}&lookup=cities`);
 
         if(response.status === 200){
             let data = await response.text();
--- world.php
+++ world.php
@@ -5,13 +5,15 @@ $password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+$num_params = count($_GET);
+
 $country = filter_input(INPUT_GET, 'query'); 
 $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%';");
-
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 <table>
+<?php if($num_params == 1): ?>
   <tr>
     <th>Name</th>
     <th>Continent</th>
@@ -26,4 +28,29 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <td><?php echo $row['head_of_state'] ?></td>
   </tr>
 <?php endforeach; ?>
+
+<?php elseif($num_params > 1):?>
+  <tr>
+    <th>Name</th>
+    <th>District</th>
+    <th>Population</th>
+  </tr>
+
+  <?php 
+    $countries_list = array();
+    foreach ($results as $row):
+      $country_code = $row['code']; 
+      // echo '<pre>'; print_r($results); echo '</pre>';
+      $stmt1 =  $conn->query("SELECT * FROM countries INNER JOIN cities ON cities.country_code = countries.code WHERE cities.country_code = '$country_code'");
+      $results1 = $stmt1->fetchAll(PDO::FETCH_ASSOC);
+    endforeach; ?>
+  <?php foreach ($results1 as $row): ?>
+  <tr>
+    <td><?php echo $row['name'] ?></td>
+    <td><?php echo $row['district'] ?></td>
+    <td><?php echo $row['population'] ?></td>
+  </tr>
+<?php endforeach; ?>
+<?php endif; ?>
+
 </table>
